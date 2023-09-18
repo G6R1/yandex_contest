1b98d61df56838089e567b9d6aa4eea637244c6c
G6R1 <gerian109@yandex.ru>
2023-09-18 21:50:45 +0300

    task 4 -сортировкабез нее лучше 14 тестов

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,25 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) {
         BufferedReader bi = new BufferedReader(new InputStreamReader(System.in));
         String line;
-        int[][] orders;
-        int[][] ordersByEnd;
+        int[] orders;
 
         try {
             int ordersNum = Integer.parseInt(bi.readLine());
-            orders = new int[ordersNum][4]; //start end cost duration
+            orders = new int[ordersNum * 4]; //start end cost duration
 
-            for (int i = 0; i < ordersNum; i++) {
+            for (int i = 0; i < ordersNum * 4; i = i + 4) {
                 line = bi.readLine();
                 String[] split = line.split(" ");
-                orders[i][0] = Integer.parseInt(split[0]);
-                orders[i][1] = Integer.parseInt(split[1]);
-                orders[i][2] = Integer.parseInt(split[2]);
-                orders[i][3] = orders[i][1] - orders[i][0];
+                orders[i] = Integer.parseInt(split[0]);
+                orders[i+1] = Integer.parseInt(split[1]);
+                orders[i+2] = Integer.parseInt(split[2]);
+                orders[i+3] = orders[i+1] - orders[i];
             }
-
-            ordersByEnd = Arrays.stream(orders).map(int[]::clone).toArray(int[][]::new);
-            Arrays.sort(orders, Comparator.comparingInt(x -> x[0]));
-            Arrays.sort(ordersByEnd, Comparator.comparingInt(x -> x[1]));
 
             int requestsNum = Integer.parseInt(bi.readLine());
 
@@ -40,18 +33,14 @@ public class Main {
                 long sum = 0;
 
                 if (type == 1) {
-                    for (int[] order : orders) {
-                        if (order[0] >= rStart && order[0] <= rEnd)
-                            sum += order[2];
-                        if (order[0] > rEnd)
-                            break;
+                    for (int j = 0; j < orders.length; j = j + 4) {
+                        if (orders[j] >= rStart && orders[j] <= rEnd)
+                            sum += orders[j + 2];
                     }
                 } else {
-                    for (int[] order : ordersByEnd) {
-                        if (order[1] >= rStart && order[1] <= rEnd)
-                            sum += order[3];
-                        if (order[1] > rEnd)
-                            break;
+                    for (int j = 0; j < orders.length; j = j + 4) {
+                        if (orders[j + 1] >= rStart && orders[j + 1] <= rEnd)
+                            sum += orders[j + 3];
                     }
                 }
 
@@ -75,7 +64,3 @@ public class Main {
 
 1000 0 1000 90 0 90
  */
-//long start = System.currentTimeMillis();
-//System.out.println("init " + (System.currentTimeMillis() - start));
-//System.out.println();
-//System.out.println("final " + (System.currentTimeMillis() - start));
